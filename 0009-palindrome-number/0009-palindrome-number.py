21e1abd6ea5a45026bcfc532b8518bfa725103b0
Alaa Khattab <alaakhattab92@gmail.com>
2023-08-29 12:10:28 +0300

    Time: 60 ms (77.00%), Space: 16.3 MB (35.89%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,19 +1,33 @@
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         if x < 0:
+#             return False
+#         if 0 <= x <= 9:
+#             return True
+#         num_as_list = str(x)
+#         num_len = len(num_as_list)
+#         i = 0
+#         j = num_len - 1
+#         for k in num_as_list:
+#             if i >= j:
+#                 return True
+#             if int(num_as_list[i]) != int(num_as_list[j]):
+#                 return False
+#             i+=1
+#             j-=1
+                
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
-        if 0 <= x <= 9:
-            return True
-        num_as_list = str(x)
-        num_len = len(num_as_list)
-        i = 0
-        j = num_len - 1
-        for k in num_as_list:
-            if i >= j:
-                return True
-            if int(num_as_list[i]) != int(num_as_list[j]):
-                return False
-            i+=1
-            j-=1
-                
+
+        reversed_num = 0
+        temp = x
+
+        while temp != 0:
+            digit = temp % 10
+            reversed_num = reversed_num * 10 + digit
+            temp //= 10
+
+        return reversed_num == x
         
