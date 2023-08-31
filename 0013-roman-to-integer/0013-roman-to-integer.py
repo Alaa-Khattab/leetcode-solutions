0a6af5182f26218a7fc068d09b0e7f6af46b93ee
Alaa Khattab <alaakhattab92@gmail.com>
2023-08-31 11:13:35 +0300

    Time: 71 ms (12.30%), Space: 16.5 MB (10.15%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,37 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        keys = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total = 0
+        as_list = list(s)
+        while len(as_list ) > 0:
+            
+            if len(as_list) == 1:
+                total += keys[as_list[0]]
+                print('11111', total)
+                break;
+            else:
+                print('----', len(as_list))
+                if keys[as_list[0]] >= keys[as_list[1]]:
+                    total += keys[as_list[0]]
+                    print('2222', total)
+                    as_list.pop(0)
+                else:
+                    total = total +   (keys[as_list[1]] - keys[as_list[0]])
+                    print('33333', total)
+                    as_list.pop(0)
+                    as_list.pop(0)
+                    
+                    
+            
+        
+        
+        return total
+        
